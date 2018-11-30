0627e699568f76866e4a229d24b10b4eb1b4ac73
zhaoangus <zhao_angus@163.com>
2018-11-30 12:00:52 +0800

    connect to wechat

---
+++ server/config/index.js
@@ -0,0 +1,7 @@
+export default {
+  wechat: {
+    appID: 'wx35250e8fcad57cbd',
+    appsecret: 'c5d1f1d6c726b7f2b65d2f4a034ed5d6',
+    token: 'zhaoangus'
+  }
+}
--- server/index.js
+++ server/index.js
@@ -1,44 +1,65 @@
-
 const Koa = require('koa')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
+import R from 'ramda'
+import { resolve } from 'path'
 
-const app = new Koa()
+// const app = new Koa()
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
+const r = path => resolve(__dirname, path)
 
 // Import and Set Nuxt.js options
 let config = require('../nuxt.config.js')
-config.dev = !(app.env === 'production')
+config.dev = !(process.env === 'production')
 
-async function start() {
-  // Instantiate nuxt.js
-  const nuxt = new Nuxt(config)
+const MIDDLEWARES = ['router']
 
-  // Build in development
-  if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
+class Server {
+  constructor () {
+    this.app = new Koa()
+    this.useMiddleWares(this.app)(MIDDLEWARES)
   }
 
-  app.use(ctx => {
-    ctx.status = 200 // koa defaults to 404 when it sees that status is unset
+  useMiddleWares (app) {
+    return R.map(R.compose(
+      R.map(i => i(app)),
+      require,
+      i => `${r('./middleware')}/$(i)`
+    ))
+  }
 
-    return new Promise((resolve, reject) => {
-      ctx.res.on('close', resolve)
-      ctx.res.on('finish', resolve)
-      nuxt.render(ctx.req, ctx.res, promise => {
-        // nuxt.render passes a rejected promise into callback on error.
-        promise.then(resolve).catch(reject)
+  async start () {
+    const nuxt = new Nuxt(config)
+
+  // Build in development
+    if (config.dev) {
+      const builder = new Builder(nuxt)
+      await builder.build()
+    }
+
+    this.app.use(ctx => {
+      ctx.status = 200 // koa defaults to 404 when it sees that status is unset
+
+      return new Promise((resolve, reject) => {
+        ctx.res.on('close', resolve)
+        ctx.res.on('finish', resolve)
+        nuxt.render(ctx.req, ctx.res, promise => {
+          // nuxt.render passes a rejected promise into callback on error.
+          promise.then(resolve).catch(reject)
+        })
       })
     })
-  })
 
-  app.listen(port, host)
-  consola.ready({
-    message: `Server listening on http://${host}:${port}`,
-    badge: true
-  })
+    this.app.listen(port, host)
+    console.ready({
+      message: `Server listening on http://${host}:${port}`,
+      badge: true
+    })
+  }
+
 }
 
-start()
+const app = new Server()
+
+app.start()
